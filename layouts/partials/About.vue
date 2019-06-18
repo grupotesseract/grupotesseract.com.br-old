bbf39a63d63e8bc475d355ef30c18416a6b8740f
Leandro Acquati <leandroacquati@gmail.com>
2019-06-18 20:40:52 -0300

    WIP: Animations on About

--- layouts/partials/About.vue
+++ layouts/partials/About.vue
@@ -1,11 +1,21 @@
 <template>
   <v-layout class="about" row wrap justify-center>
     <v-flex xs12>
-      <div class="title-line" :style="{ width: bottomTitle + 'px' }"></div>
+      <div class="topTitleLine" :style="{ width: bottomTitleSize + 'px' }">
+        <div v-waypoint="{ active: true, callback: topTitleLine }"></div>
+      </div>
     </v-flex>
     <v-flex xs12 sm12 md4 lg3 xl2>
-      <h1 v-waypoint="{ active: true, callback: logOne }">QUEM</h1>
-      <h2 ref="bottomTitleRef">SOMOS</h2>
+      <h1 v-waypoint="{ active: true, callback: topTitle }" class="topTitle">
+        QUEM
+      </h1>
+      <h2
+        ref="bottomTitleRef"
+        v-waypoint="{ active: true, callback: bottomTitle }"
+        class="bottomTitle"
+      >
+        SOMOS
+      </h2>
     </v-flex>
     <v-flex xs12 sm12 md6 lg5 xl4>
       <p>
@@ -14,7 +24,8 @@
         serviços dos mais variados tipos, como: desenvolvimento de sites,
         aplicativos e soluções de infraestrutura.
       </p>
-      <button style="color: white;" @click="getPos()">Button</button>
+      <p>Width: {{ window.width }}</p>
+      <p>Height: {{ window.height }}</p>
     </v-flex>
   </v-layout>
 </template>
@@ -23,16 +34,62 @@
 export default {
   data() {
     return {
-      bottomTitle: 0
+      bottomTitleSize: 0,
+      window: {
+        width: 0,
+        height: 0
+      }
+    }
+  },
+  created() {
+    this.handleResize()
+  },
+  destroyed() {
+    // eslint-disable-next-line nuxt/no-env-in-hooks
+    if (process.client) {
+      window.removeEventListener('resize', this.handleResize)
     }
   },
   mounted() {
-    this.bottomTitle =
-      this.$refs.bottomTitleRef.getBoundingClientRect().left + 248
+    if (this.window.width <= 599) {
+      this.bottomTitleSize =
+        this.$refs.bottomTitleRef.getBoundingClientRect().left + 264
+    } else if (this.window.width >= 600 && this.window.width <= 959) {
+      this.bottomTitleSize =
+        this.$refs.bottomTitleRef.getBoundingClientRect().left + 280
+    } else {
+      this.bottomTitleSize =
+        this.$refs.bottomTitleRef.getBoundingClientRect().left + 248
+    }
+    // eslint-disable-next-line nuxt/no-env-in-hooks
+    if (process.client) {
+      window.addEventListener('resize', this.handleResize)
+    }
   },
   methods: {
-    logOne({ el, going, direction }) {
-      el.classList.toggle('active', this.$waypointMap.GOING_IN === going)
+    topTitleLine({ el, going, direction }) {
+      el.classList.toggle(
+        'topTitleLineActive',
+        this.$waypointMap.GOING_IN === going
+      )
+    },
+    topTitle({ el, going, direction }) {
+      el.classList.toggle(
+        'topTitleActive',
+        this.$waypointMap.GOING_IN === going
+      )
+    },
+    bottomTitle({ el, going, direction }) {
+      el.classList.toggle(
+        'bottomTitleActive',
+        this.$waypointMap.GOING_IN === going
+      )
+    },
+    handleResize() {
+      if (process.client) {
+        this.window.width = window.innerWidth
+        this.window.height = window.innerHeight
+      }
     }
   }
 }
@@ -42,10 +99,23 @@ export default {
 .about {
   padding-bottom: 8rem;
 
-  .title-line {
-    height: 2px;
-    background-color: $blue-1;
+  .topTitleLine {
+    height: 3px;
     margin-bottom: 2rem;
+
+    div {
+      width: 0%;
+      height: 3px;
+      background-color: $blue-1;
+    }
+  }
+
+  .topTitle {
+    opacity: 0;
+  }
+
+  .bottomTitle {
+    opacity: 0;
   }
 
   h1, h2, p {
@@ -96,7 +166,51 @@ export default {
   }
 }
 
-.active {
-  border: 2px dashed white;
+.topTitleLineActive {
+  animation-name: growLine;
+  animation-duration: 2.5s;
+  animation-timing-function: ease;
+  animation-fill-mode: forwards;
+
+  @keyframes growLine {
+    from { width: 0%; }
+    to { width: 100%; }
+  }
+}
+
+.topTitleActive {
+  animation-name: slideRight;
+  animation-duration: 2.5s;
+  animation-timing-function: ease;
+  animation-fill-mode: forwards;
+
+  @keyframes slideRight {
+    from {
+      transform: translate(-80px);
+      opacity: 0;
+    }
+    to {
+      transform: translate(none);
+      opacity: 1;
+    }
+  }
+}
+
+.bottomTitleActive {
+  animation-name: slideLeft;
+  animation-duration: 2.5s;
+  animation-timing-function: ease;
+  animation-fill-mode: forwards;
+
+  @keyframes slideLeft {
+    from {
+      transform: translate(80px);
+      opacity: 0;
+    }
+    to {
+      transform: translate(none);
+      opacity: 1;
+    }
+  }
 }
 </style>
