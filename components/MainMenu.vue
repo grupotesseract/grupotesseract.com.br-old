da6970eb0c17d511b87f9dc93ba0653060c89b8d
Evandro Carreira <evandro.carreira@gmail.com>
2019-06-25 19:45:03 -0300

    Menu com animações simples

--- components/MainMenu.vue
+++ components/MainMenu.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="main-menu">
-    <v-toolbar fixed app dark color="transparent">
+  <div>
+    <v-toolbar fixed app dark flat color="transparent">
       <!-- <v-avatar>
         <img
           src=""
@@ -17,18 +17,30 @@
         <v-icon>menu</v-icon>
       </v-btn>
     </v-toolbar>
-    <v-navigation-drawer v-model="showMenu" right temporary fixed dark>
-      <v-list>
+
+    <v-navigation-drawer
+      v-model="showMenu"
+      class="navigation"
+      right
+      temporary
+      fixed
+    >
+      <v-list class="main-menu">
         <v-list-tile
           v-for="(item, i) in items"
           :key="i"
           v-scroll-to="item.to"
+          class="item-menu"
+          active-class="item-menu-active"
           :to="item.to"
           router
           exact
         >
           <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+            <v-list-tile-title
+              class="title-menu"
+              v-text="item.title"
+            ></v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
@@ -40,7 +52,7 @@
 export default {
   data() {
     return {
-      showMenu: false,
+      showMenu: null,
       items: [
         {
           title: 'Home',
@@ -68,16 +80,57 @@ export default {
 .v-toolbar {
   background-color : red
 }
+.title-menu {
+  text-align: right;
+  width: auto;
+  overflow: visible;
+}
+.title-menu::before {
+  content: '';
+  width: 0;
+  height: 2px;
+  display: block;
+  position: absolute;
+  right: 115%;
+  top: 40%;
+  background-color: $blue-1;
+  transition: ease all 0.5s;
+}
+
+aside.navigation {
+  background-color: transparent;
+  box-shadow: none;
+  height: auto !important;
+  margin-top: 4em !important;
+  padding-right: 1em;
+}
+
 .main-menu {
   color: red;
   font-family: jaapokki-regular;
 }
 
-a.v-list__tile {
-  color: magenta;
+.item-menu {
+  color: white;
+  transition: ease 0.3s all;
 }
 
-a.v-list__tile--active {
-  color: green;
+.item-menu:hover {
+  color: $blue-1;
+  .title-menu::before {
+    width: 2em;
+  }
+}
+
+.item-menu > a {
+  height: 0 !important;
+}
+
+.main-menu:hover .item-menu a {
+  height: 100%;
+}
+
+.item-menu-active div{
+  color: $blue-1;
 }
 </style>
