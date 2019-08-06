260b0d07530dbe9f8214f4d36d6eb9dc04115079
Leandro Acquati <leandroacquati@gmail.com>
2019-08-06 01:38:21 -0300

    add desktop title layout

--- layouts/partials/Contact.vue
+++ layouts/partials/Contact.vue
@@ -1,9 +1,15 @@
 <template>
-  <v-layout class="contact">
-    <v-flex>
-      <p>Quer criar um projeto com a gente?</p>
-    </v-flex>
-  </v-layout>
+  <div class="contact">
+    <v-layout>
+      <v-flex xs12 md6 class="title">
+        <v-layout align-end justify-center column fill-height>
+          <h3>QUER CRIAR</h3>
+          <h2>UM PROJETO</h2>
+          <h3>COM A GENTE?</h3>
+        </v-layout>
+      </v-flex>
+    </v-layout>
+  </div>
 </template>
 
 <script>
@@ -16,12 +22,26 @@ export default {
 
 <style lang="stylus" scoped>
 .contact {
-  width: 100%;
-  height: 400px;
+  padding: 4rem 0rem;
   background-color: $blue-2;
 
-  p {
-    color: $black-1;
+  .title {
+    height: 500px;
+    border-right: 3px solid $gray-1;
+
+    h2, h3 {
+      margin-right: 3rem;
+    }
+
+    h3 {
+      color: $black-1;
+    }
+
+    h2 {
+      -webkit-text-fill-color: $blue-2;
+      -webkit-text-stroke-width: 2px;
+      -webkit-text-stroke-color: $black-1;
+    }
   }
 }
 </style>
