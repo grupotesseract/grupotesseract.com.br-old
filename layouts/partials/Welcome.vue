aa62ff23954569162fb0e92d685c3bd7f460c921
Renato Gomes <renatoo.agomes@gmail.com>
2019-08-07 20:32:13 -0300

    incluindo lottie e usando ele para dar play na animacao

--- layouts/partials/Welcome.vue
+++ layouts/partials/Welcome.vue
@@ -1,26 +1,41 @@
 <template>
   <v-layout class="welcome">
     <v-flex>
-      <p>_ Tesseract</p>
+      <lottie :options="defaultOptions" @animCreated="handleAnimation" />
     </v-flex>
   </v-layout>
 </template>
 
 <script>
+import Lottie from '~/components/Lottie.vue'
+import animationData from '~/assets/animacao.json'
+
 export default {
+  components: {
+    lottie: Lottie
+  },
   data() {
-    return {}
+    return {
+      defaultOptions: {
+        animationData: animationData,
+        loop: false
+      }
+    }
+  },
+  methods: {
+    handleAnimation: function(anim) {
+      this.anim = anim
+    }
   }
 }
 </script>
 
 <style lang="stylus" scoped>
 .welcome {
-  width: 100%;
-  height: 400px;
+  min-height: 90vh;
 
-  p {
-    color: $white-1;
+  @media (max-width: 960px) {
+    min-height: 40vh;
   }
 }
 </style>
