0a9aeee6764f61e41ab930286c92c4cd091321e4
Renato Gomes <renatoo.agomes@gmail.com>
2019-08-07 01:13:31 -0300

    adicionando links das midias e responsividade

--- layouts/partials/Footer.vue
+++ layouts/partials/Footer.vue
@@ -6,25 +6,29 @@
         Tesseract
       </h3>
       <v-flex xs12 class="social-nav">
-        <a href="https://instagram.com/grupotesseract">
-          <fa :icon="fab.faInstagram" class="fa-3x" />
+        <a target="blank" href="https://instagram.com/grupotesseract">
+          <fa :icon="fab.faInstagram" class="fa-2x" />
         </a>
-        <a href="https://fb.com/grupotesseract">
-          <fa :icon="fab.faFacebookSquare" class="fa-3x" />
+        <a target="blank" href="https://fb.com/grupotesseract">
+          <fa :icon="fab.faFacebookSquare" class="fa-2x" />
         </a>
-        <a href="https://github.com/grupotesseract">
-          <fa :icon="fab.faGithub" class="fa-3x" />
+        <a target="blank" href="https://github.com/grupotesseract">
+          <fa :icon="fab.faGithub" class="fa-2x" />
         </a>
-        <a href="https://medium.com/grupo-tesseract">
-          <fa :icon="fab.faMedium" class="fa-3x" />
+        <a target="blank" href="https://medium.com/grupo-tesseract">
+          <fa :icon="fab.faMedium" class="fa-2x" />
         </a>
-        <a href="https://www.linkedin.com/company/grupotesseract/">
-          <fa :icon="fab.faLinkedin" class="fa-3x" />
+        <a target="blank" href="https://linkedin.com/company/grupotesseract">
+          <fa :icon="fab.faLinkedin" class="fa-2x" />
         </a>
-        <a href="https://wa.me/5514981225509">
-          <fa :icon="fab.faWhatsapp" class="fa-3x" />
+        <a target="blank" href="https://wa.me/5514981225509">
+          <fa :icon="fab.faWhatsapp" class="fa-2x" />
         </a>
-        <a href="mailto:contato@grupotesseract.com.br">
+        <a
+          class="mailto_link"
+          target="_blank"
+          href="mailto:contato@grupotesseract.com.br"
+        >
           <span>contato@grupotesseract.com.br</span>
         </a>
       </v-flex>
@@ -48,13 +52,24 @@ export default {
 
 <style lang="stylus" scoped>
 .footer {
-  padding: 5rem 4.2rem ;
+  padding: 1rem;
+  text-align: center;
+  height: 330px;
+
+  @media (min-width: 960px) {
+    text-align:left;
+    padding: 4.5rem 3.5rem;
+    height: 210px;
+  }
+
   width: 100%;
-  height: 280px;
   background-color: $gray-2;
 
   h3 {
     font-size: 2.7rem;
+    @media (max-width: 960px) {
+      margin-bottom:2rem;
+    }
     span {
       letter-spacing:-7px;
       font-size: 1.8rem;
@@ -66,7 +81,25 @@ export default {
   }
 
   .social-nav {
-    padding: 2.5rem;
+    padding: 1rem;
+    @media (max-width: 960px) {
+      padding: 0rem;
+    }
+
+    .mailto_link {
+      width: 100%;
+
+      @media (max-width: 960px) {
+        margin-top:3rem;
+        display:block;
+
+      }
+    }
+
+    @media (min-width: 960px) {
+      padding: 2.5rem;
+      margin-top: none;
+    }
 
     a {
       color: white;
@@ -75,6 +108,10 @@ export default {
 
       span {
         font-size:1.5rem;
+
+        @media (max-width: 960px) {
+          font-size:1.2rem;
+        }
       }
     }
   }
